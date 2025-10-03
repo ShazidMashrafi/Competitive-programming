a4f62fee3ae20f4c6efd1200b146fdee3de9d294
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-10-04 01:49:07 +0600

    implement Trie data structure for maximum XOR calculation

---
+++ Maximum XOR of Two Numbers in an Array/code.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Trie {
+private:
+    struct TrieNode {
+        TrieNode* children[2];
+        
+        TrieNode() {
+            for (int i = 0; i < 2; i++)
+                children[i] = nullptr;
+        }
+    };
+    
+    TrieNode* root;
+    
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(int num) {
+        TrieNode* node = root;
+        for(int i = 31; i >= 0; --i) {
+            int bit = (num >> i) & 1;
+            if(!node->children[bit]) {
+                node->children[bit] = new TrieNode();
+            }
+            node = node->children[bit];
+        }
+    }
+
+    int getMaxXOR(int num) {
+        TrieNode* node = root;
+        int mx = 0;
+        for(int i = 31; i >= 0; --i) {
+            int bit = (num >> 1) & 1;
+            int op = 1 - bit;
+            if(node->children[op]) {
+                mx |= (1 << i);
+                node = node->children[op];
+            } else {
+                node = node->children[bit];
+            }
+        }
+        return mx;
+    }
+};
+
+class Solution {
+public:
+    Trie trie;
+    int ans = 0;
+    int findMaximumXOR(vector<int>& nums) {
+        for(int num : nums) {
+            trie.insert(num);
+        }
+        for(int num : nums) {
+            ans = max(ans, trie.getMaxXOR(num));
+        }
+
+        return ans;
+    }
+};
